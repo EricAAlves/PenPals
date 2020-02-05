8932b817e7123447d682e94f6f47f9016f82d12a
Eric Alves <ericantunesalves@gmail.com>
2020-02-05 10:23:10 -0500

    Initial Commit

---
+++ PenPals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PenPals
+//
+//  Created by Eric Alves on 2/5/20.
+//  Copyright © 2020 Team1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
